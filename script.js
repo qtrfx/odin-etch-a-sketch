7cfe5f5767bfa28b1bd240861a9d3aaea51d2fcd
qtrfx <24359705+qtrfx@users.noreply.github.com>
2024-10-08 23:46:29 +0200

    feat: add named functions for handling events

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ const gridContainer = document.querySelector(".app-container");
 const changeTileSizeButton = document.querySelector(".change-tile-size-button");
 const resetSketchButton = document.querySelector(".reset-sketch-button");
 
+const GRID_DIMENSIONS = 960;
+
 let tileSize = 50;
 
 function drawGrid(tileSize) {
-    if (parseInt(tileSize) > 100 || parseInt(tileSize) < 1) {
-        return;
-    }
     gridContainer.replaceChildren();
     for (let i = 0; i < tileSize * tileSize; i++) {
         const gridTile = document.createElement("div");
         gridTile.classList.add("grid-tile");
-        gridTile.style.height = `${960 / tileSize}px`;
-        gridTile.style.width = `${960 / tileSize}px`;
+        gridTile.style.height = `${GRID_DIMENSIONS / tileSize}px`;
+        gridTile.style.width = `${GRID_DIMENSIONS / tileSize}px`;
         gridContainer.appendChild(gridTile);
     }
 }
@@ -26,12 +25,30 @@ function resetGrid() {
         tile.classList.remove("blue");
     });
 }
+
+function changeTileSize(event) {
+    let gridSizePrompt = "Enter your desired grid size. (1-100)";
+
+    while (true) {
+        const userInput = parseInt(prompt(gridSizePrompt));
+        if (userInput <= 100 && userInput > 0) {
+            tileSize = userInput;
+            break;
+        } else {
+            gridSizePrompt =
+                "Invalid Input, please input a number between 1-100.";
+        }
+    }
+
+    drawGrid(tileSize);
+}
+
+function tintTile(event) {
     if (event.target.className === "grid-tile") {
         event.target.classList.add("blue");
     }
 }
 
-changeTileSizeButton.addEventListener("click", (event) => {
-    tileSize = prompt("Enter your desired grid size: (1-100)");
-    drawGrid(tileSize);
-});resetSketchButton.addEventListener("click", resetGrid);
+gridContainer.addEventListener("mouseover", tintTile);
+changeTileSizeButton.addEventListener("click", changeTileSize);
+resetSketchButton.addEventListener("click", resetGrid);
